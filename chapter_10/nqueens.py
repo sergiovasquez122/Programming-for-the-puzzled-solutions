8183ebfbe108a3bf5d85e57f1f2f4f7dad685ec8
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-12-23 15:34:32 -0800

    added code segments

---
+++ chapter_10/nqueens.py
@@ -0,0 +1,35 @@
+#This procedure initializes the board to be empty, calls the recursive N-queens
+#procedure and prints the returned solution
+def nQueens(size):
+    board = [-1] * size
+    rQueens(board, 0, size)
+    print (board)
+
+#This procedure checks that the most recently placed queen on column current
+#does not conflict with queens in columns to the left.
+def noConflicts(board, current):
+    for i in range(current):
+        if (board[i] == board[current]):
+            return False
+        if (current - i == abs(board[current] - board[i])):
+            return False
+    return True
+
+
+#This procedure places a queens on the board on a given column so it does
+#not conflict with the existing queens, and then calls itself recursively
+#to place subsequent queens till the requisite number of queens are placed
+def rQueens(board, current, size):
+    if (current == size):
+        return True
+    else:
+        for i in range(size):
+            board[current] = i
+            if (noConflicts(board, current)):
+                done = rQueens(board, current + 1, size)
+                if (done):
+                    return True
+        return False
+
+
+nQueens(20)
